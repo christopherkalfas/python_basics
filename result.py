4bcf1e3884d179f4308f9aa0f0aeee4081a543d5
Chris Kalfas <christopherkalfas22@gmail.com>
2020-04-11 16:11:37 -0400

    Continued practicing try and except conditionals

---
+++ result.py
@@ -0,0 +1,23 @@
+print("A")
+try:
+    result = 5 + 5
+    print("B")
+except ValueError:
+    print("C")
+except TypeError:
+    print("D")
+else:
+    print("E")
+print("F")
+
+print("A")
+try:
+    result = "test" + 5
+    print("B")
+except ValueError:
+    print("C")
+except TypeError:
+    print("D")
+else:
+    print("E")
+print("F")
